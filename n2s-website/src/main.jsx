79bb668607c9be5b3345664fde41d60c59bac3f6
Nícolas Queiroga <nicolasqueirogapaix@gmail.com>
2025-10-15 00:29:39 -0300

    Resolve merge conflicts: remove duplicate routes and code

--- n2s-website/src/main.jsx
+++ n2s-website/src/main.jsx
@@ -3,14 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home';
-import ContactPage from './pages/Contact';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contato" element={<ContactPage />} />
+        <Route path="/contato" element={<Contact />} />
+        
+        {/* Rota para a página Services */}
+        <Route path="/services" element={<Services />} />
       </Routes>
     </Router>
   </StrictMode>,
